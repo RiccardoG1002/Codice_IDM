d470358c998c53add8e4ddccb1dfde13b3b5c06b
RiccardoG1002 <81828547+RiccardoG1002@users.noreply.github.com>
2021-04-13 10:17:53 +0200

    aaa

--- FerrovieDelloStato/src/ComponentiTreni/VagoneBagno.java
+++
@@ -1,9 +0,0 @@
-package ComponentiTreni;
-
-public abstract class VagoneBagno extends Vagone {
-	public final void pulisci() {
-		this.pulisciPersonalizzato();
-	}
-	
-	protected abstract void pulisciPersonalizzato();
-}

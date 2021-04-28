0e2100e62dd89908b85a4cdf1411e4c54bf4b19d
RiccardoG1002 <81828547+RiccardoG1002@users.noreply.github.com>
2021-04-28 09:10:46 +0200

    new navbar

--- CheckString/src/com/corso/checkstring/base/Synonyms.java
+++ CheckString/src/com/corso/checkstring/base/Synonyms.java
@@ -1,8 +1,13 @@
 package com.corso.checkstring.base;
 
 import java.util.Collection;
+import java.util.List;
+
 import com.corso.checkstring.beans.Country;
+import com.corso.checkstring.beans.Id;
 import com.corso.checkstring.beans.Pattern;
+import com.corso.checkstring.dao.CountryDAO;
+import com.corso.checkstring.dao.CountryDAOImpl;
 public class Synonyms {
 
 	public Collection<Pattern> findSynonyms(Country country) {
